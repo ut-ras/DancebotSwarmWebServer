e8c13e01fa33cd684fb1e9e5f4724079381cd642
amadraccoon101 <amadraccoon@gmail.com>
2020-08-18 11:57:13 -0500

    working on issue 1, stuck on how to get dict keys from json

--- index.js
+++ index.js
@@ -20,12 +20,34 @@ fs.writeFile('output.json', obj, (err) => {
 // open up the endpoint /robotJoin to a GET request
 app.get('/robotJoin', (req, res) => {
     console.log("req query: ", req.query);
-
+    var exists = false;
     let output = getDataFromFile();
+    if(req.query['id'] != null){
+        var botlist = output.robots; //how to get key from robots?
+        var i = 0;
+        for(i = 0; i < output.num_connected; i++){
+            let robot = botlist[i];
+            console.log(robot);
+            console.log(botlist);
+            if(i === (req.query['id'])){
+                console.log('successful find!');
+                exists = true;
+            }
+        }
+    }
     // if this is the first robot who connects, let's set him up as something random.
-    if(output.num_connected == 0){
+    if(req.query['id'] == null){
+        console.log('Dame da ne, dame yo, dame dano yo');
         output.num_connected ++;
-        var ROBOT_ID = 72;
+        var auto = 'auto_add';
+        var ROBOT_ID = auto.concat(output.num_connected.toString());
+        var duck = `{"${ROBOT_ID}": {"robot_state": "rose","robot_eye_color": "photo","robot_expression": "light"}}`;
+        output.robots.push(JSON.parse(duck));
+        saveDataToFile(output);
+    }
+    else if(!exists){
+        output.num_connected ++;
+        var ROBOT_ID = req.query['id'];
         var duck = `{"${ROBOT_ID}": {"robot_state": "rose","robot_eye_color": "photo","robot_expression": "light"}}`;
         output.robots.push(JSON.parse(duck));
         saveDataToFile(output);
